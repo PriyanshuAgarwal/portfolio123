7c93ebd2a4be2bb0d7160a77eecaddd65ff998e6
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2020-06-28 14:21:32 +0530

    Add Personal details

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -17,7 +17,35 @@ class AboutMe extends Component {
 
                             <p>Consectetur consectetur nesciunt nesciunt blanditiis amet? Quod quis unde aliquid excepturi laudantium? Illo architecto aliquam velit illo magnam neque perferendis sint Incidunt eius ipsa inventore labore eos velit. Minus hic magni reiciendis necessitatibus ex Quidem officia deleniti accusamus obcaecati dolores. Porro culpa quibusdam impedit porro praesentium excepturi. Alias ab consequatur? </p>
                         </div>
-                        <div className="about-personal-details"></div>
+                        <div className="about-personal-details">
+                            <h3 className="mb-20">Personal Information</h3>
+                            <ul>
+                                <li>
+                                    <span className="title">Name </span>
+                                    <span className="value"> Priyanshu Agarwal</span>
+                                </li>
+                                <li>
+                                    <span className="title">Age </span>
+                                    <span className="value"> 27 years</span>
+                                </li>
+                                <li>
+                                    <span className="title">Residence </span>
+                                    <span className="value">Hyderabad, India</span>
+                                </li>
+                                <li>
+                                    <span className="title">Email </span>
+                                    <span className="value">priyanshuag.iet@gmail.com</span>
+                                </li>
+                                <li>
+                                    <span className="title">Phone </span>
+                                    <span className="value">(+91) 8755191368</span>
+                                </li>
+                                <li>
+                                    <span className="title">Freelance </span>
+                                    <span className="value">Available</span>
+                                </li>
+                            </ul>
+                        </div>
                     </div>      
                 </div>
                 
