3f95acb4f5b7f2eaef745961a96fe239d6882b1a
PriyanshuAgarwal <agarwal.iet888@gmail.com>
2020-08-30 23:54:42 +0530

    Add link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,13 +8,13 @@ render() {
   return (
     <div className="footer-container">
       <div className="footer-items">
-        <a href="#">
+        <a target="_blank" href="https://www.facebook.com/priyanshu.agarwal.7">
           <i className="fa fa-facebook-square" aria-hidden="true"></i>
         </a>
-        <a href="#">
-          <i className="fa fa-twitter-square" aria-hidden="true"></i>
+        <a target="_blank" href="https://github.com/PriyanshuAgarwal">
+          <i className="fa fa-github-square" aria-hidden="true"></i>
         </a>
-        <a href="#">
+        <a target="_blank" href="https://linkedin.com/in/priyanshu1960">
           <i className="fa fa-linkedin-square" aria-hidden="true"></i>
         </a>
       </div>
